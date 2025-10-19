be3bbc0602956635d425d82d729f8ae5eabda8f1
Abhinand D Manoj <abhinand.dmanoj@gmail.com>
2025-10-19 13:18:45 +0530

    rewrite error functions

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -1,25 +1,67 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"log/slog"
 	"net/http"
+	"runtime/debug"
+	"strings"
+
+	"github.com/VergilX/my-space/internal/response"
+	"github.com/VergilX/my-space/internal/validator"
 )
 
-func (app *application) cookieNotFoundError(w http.ResponseWriter, r *http.Request, err error, msg string) {
-	app.logger.Error(err.Error(), "desc", msg, "url", r.URL)
-	http.Error(w, "Bad Request", http.StatusBadRequest)
+func (app *application) logError(r *http.Request, err error) {
+	var (
+		message = err.Error()
+		method  = r.Method
+		url     = r.URL.String()
+		trace   = string(debug.Stack()) // idk if I want this
+	)
+
+	requestAttrs := slog.Group("request", "method", method, "url", url)
+	app.logger.Error(message, requestAttrs, "trace", trace)
 }
 
-func (app *application) jsonParseError(w http.ResponseWriter, r *http.Request, err error) {
-	app.logger.Error(err.Error(), "desc", "could not parse input JSON", "url", r.URL)
-	http.Error(w, "Internal JSON conversion error", http.StatusInternalServerError)
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message string, headers http.Header) {
+	message = strings.ToUpper(message[:1]) + message[1:]
+
+	err := response.JSONWithHeaders(w, status, envelope{"error": message}, headers)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
-func (app *application) formValueError(w http.ResponseWriter, r *http.Request, msg string) {
-	app.logger.Error("Bad Request", "desc", msg, "url", r.URL)
-	http.Error(w, "Bad Request", http.StatusBadRequest)
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	message := "It's civil war in here!"
+	app.errorResponse(w, r, http.StatusInternalServerError, message, nil)
 }
 
-func (app *application) authenticationError(w http.ResponseWriter, r *http.Request, err error, msg string) {
-	app.logger.Error(err.Error(), "desc", msg, "url", r.URL)
-	http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	message := "your request stinks!"
+	app.errorResponse(w, r, http.StatusBadRequest, message, nil)
+}
+
+func (app *application) methodNotAllowedError(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message, nil)
+}
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, r *http.Request, v *validator.Validator) {
+	app.logError(r, errors.New("failed validation"))
+
+	// you need to give information to the frontend so give validator
+	err := response.JSON(w, http.StatusUnprocessableEntity, v)
+	if err != nil {
+		app.logError(r, err)
+		app.serverError(w, r, err)
+	}
 }
