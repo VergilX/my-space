afad9d71e22ab1a7cfc3065326911036e6e8d8e1
Abhinand D Manoj <abhinand.dmanoj@gmail.com>
2025-10-19 13:18:23 +0530

    added validator internal lib

---
+++ internal/validator/validator.go
@@ -0,0 +1,29 @@
+package validator
+
+type Validator struct {
+	Errors map[string]string
+}
+
+func New() *Validator {
+	return &Validator{make(map[string]string)}
+}
+
+func (v *Validator) Vaild() bool {
+	return len(v.Errors) == 0
+}
+
+func (v *Validator) AddError(key, message string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = message
+	}
+}
+
+// adds validation if error is not ok
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
+
+// regex check would be really useful
+// add if needed
